2cca5f7a305e26fa0d4378b132b69d01492492d3
Alicija Bulota <aabulota@smcm.edu>
2015-03-02 17:33:57 -0800

    added model space for picture, birthday and phone number

--- dimager/models.py
+++ dimager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImagerProfile(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_image', blank=True)
+    birthday = models.DateField('date of birth', null=True)
+    phone = models.PhoneNumberField('phone number', unique=True, db_index=True)
+    
+
+    def __unicode__(self):
+        return self.user.username
+
+
+class Variable(models.Model):
+    is_active = models.BooleanField(default=True)
+
