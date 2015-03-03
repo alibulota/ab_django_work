4d37afaa998fff8822dd10005dee781f7b4add73
Alicija Bulota <aabulota@smcm.edu>
2015-03-03 13:46:01 -0800

    extended the existing user model

--- dimager/admin.py
+++ dimager/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from dimager.models import ImagerProfile
+
+
+class ImagerProfileInline(admin.StackedInline):
+    model = ImagerProfile
+    can_delete = False
+    verbose_name_plural = 'ImagerProfile'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (ImagerProfileInline, )
 
 
 class profileAdmin(admin.ModelAdmin):
     list_display = ('user', 'is_active')
 
 
-admin.site.register(ImagerProfile)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
