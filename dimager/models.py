eb800febe62ed81ea2a03316adf00a6240aa58c4
Jakealope <jake.anderson486@gmail.com>
2015-03-03 10:12:17 -0800

    added privacy fields

--- dimager/models.py
+++ dimager/models.py
@@ -5,12 +5,15 @@ from django.core.validators import RegexValidator
 
 class ImagerProfile(models.Model):
     user = models.OneToOneField(User)
+
     picture = models.ImageField(upload_to='profile_image', blank=True, max_length=100)
+    picture_privacy = models.BooleanField(default=True)
+
     birthday = models.DateField('date of birth', null=True)
+
     phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15)
-
 
     def __unicode__(self):
         return self.user.username
