d67291acd6a5663533476024583535799f1c3f36
Jakealope <jake.anderson486@gmail.com>
2015-03-03 12:43:15 -0800

    finished privacy stuff

--- dimager/models.py
+++ dimager/models.py
@@ -10,10 +10,15 @@ class ImagerProfile(models.Model):
     picture_privacy = models.BooleanField(default=True)
 
     birthday = models.DateField('date of birth', null=True)
+    birthday_privacy = models.BooleanField(default=True)
 
     phone_regex = RegexValidator(
         regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15)
+    phone_privacy = models.BooleanField(default=True)
+
+    name_privacy = models.BooleanField(default=True)
+    email_privacy = models.BooleanField(default=True)
 
     def __unicode__(self):
         return self.user.username
