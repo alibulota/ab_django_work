33544d98b16c5d76ee8e8c5cd1491f782314e1cc
Alicija Bulota <aabulota@smcm.edu>
2015-03-02 18:10:29 -0800

    updated database entry feilds

--- dimager/models.py
+++ dimager/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
 class ImagerProfile(models.Model):
     user = models.OneToOneField(User)
-    picture = models.ImageField(upload_to='profile_image', blank=True)
+    picture = models.ImageField(upload_to='profile_image', blank=True, max_length=100)
     birthday = models.DateField('date of birth', null=True)
-    phone = models.PhoneNumberField('phone number', unique=True, db_index=True)
-    
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15)
+
 
     def __unicode__(self):
         return self.user.username
 
 
-class Variable(models.Model):
-    is_active = models.BooleanField(default=True)
-
+# class Variable(models.Model):
+#     is_active = models.BooleanField(default=True)
