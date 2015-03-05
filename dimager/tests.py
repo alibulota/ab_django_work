136520c5e03ea5c442da45d61684e46450da9e51
Alicija Bulota <aabulota@smcm.edu>
2015-03-04 22:05:19 -0800

    added tests for following and blocking relationships

--- dimager/tests.py
+++ dimager/tests.py
@@ -8,7 +8,7 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
         django_get_or_create = ('username',)
-    username = 'Butters'
+    username = 'elenore'
 
 
 class TestCase_ImagerProfile(TestCase):
@@ -27,3 +27,42 @@ class TestCase_ImagerProfile(TestCase):
     def test_is_active(self):
         '''Test that user is active user'''
         assert self.test_user.ImagerProfile.is_active()
+
+
+class TestCase_FollowBlock(TestCase):
+    def setUp(self):
+        self.elenore = UserFactory()
+        self.rigby = UserFactory(username='rigby')
+
+    def test_followers(self):
+        '''Test for follower relationship'''
+        self.elenore.ImagerProfile.follow(self.rigby.ImagerProfile)
+        self.assertIn(self.elenore.ImagerProfile, self.rigby.ImagerProfile.followers.all())
+
+    def test_following(self):
+        '''Test for following relationship'''
+        self.elenore.ImagerProfile.follow(self.rigby.ImagerProfile)
+        self.assertIn(self.elenore.ImagerProfile, self.rigby.ImagerProfile.following.all())
+
+    def test_follow(self):
+        '''Test for ability to follow'''
+        self.elenore.ImagerProfile.follow(self.rigby.ImagerProfile)
+        self.assertIn(self.elenore.ImagerProfile, self.rigby.ImagerProfile.follow.all())
+
+    def test_unfollow(self):
+        self.elenore.ImagerProfile.follow(self.rigby.ImagerProfile)
+        self.rigby.ImagerProfile.unfollow(self.elenore.ImagerProfile)
+        self.assertNotIn(self.rigby.ImagerProfile, self.elenore.ImagerProfile.follow())
+
+    def test_blcok(self):
+        self.elenore.ImagerProfile.follow(self.rigby.ImagerProfile)
+        self.rigby.ImagerProfile.block(self.elenore.ImagerProfile)
+        self.assertNotIn(self.rigby.ImagerProfile, self.elenore.ImagerProfile.following())
+
+    def test_unblock(self):
+        '''Test ability to unblock user profile'''
+        self.elenore.ImagerProfile.follow(self.rigby.ImagerProfile)
+        self.rigby.ImagerProfile.block(self.elenore.ImagerProfile)
+        self.assertNotIn(self.rigby.ImagerProfile, self.elenore.ImagerProfile.following())
+        self.rigby.ImagerProfile.unblock(self.elenore.ImagerProfile)
+        self.assertIn(self.rigby.ImagerProfile, self.elenore.ImagerProfile.following())
