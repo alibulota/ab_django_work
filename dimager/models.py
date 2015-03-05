3698ee5e32b582a22d1cf0d0fe805e65e4e4f67b
Alicija Bulota <aabulota@smcm.edu>
2015-03-04 20:32:56 -0800

    added functions to block, unblock, follow, unfollow, list follower, list following. added doc strings

--- dimager/models.py
+++ dimager/models.py
@@ -5,6 +5,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 
 class ActiveImagerManager(models.Manager):
+    '''Return active users, inherit query set'''
     def get_queryset(self):
         qs = super(ActiveImagerManager, self).get_queryset()
         return qs.filter(user__is_active__exact=True)
@@ -12,6 +13,7 @@ class ActiveImagerManager(models.Manager):
 
 @python_2_unicode_compatible
 class ImagerProfile(models.Model):
+    '''Expanding user profile'''
 
     user = models.OneToOneField(User)
     following = models.ManyToManyField(
@@ -26,7 +28,8 @@ class ImagerProfile(models.Model):
     birthday_privacy = models.BooleanField(default=True)
 
     phone_regex = RegexValidator(
-        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+        regex=r'^\+?1?\d{9,15}$', message=(
+            "Entered in the format: '+999999999'. Up to 15 digits allowed."))
     phone_number = models.CharField(
         validators=[phone_regex], blank=True, max_length=15)
     phone_privacy = models.BooleanField(default=True)
@@ -37,19 +40,33 @@ class ImagerProfile(models.Model):
     def is_active(self):
         return self.user.is_active
 
-    def __unicode__(self):
+    def __str__(self):
         return self.user.username
+
+    objects = models.Manager()
+    active = ActiveImagerManager()
 
     def followers(self):
         '''List all users followers'''
-        all_followers = Following.objects.sent_requests(user=request.user)
+        return ImagerProfile.objects.filter(following__exact=self)
+
     def following(self):
-        '''List of profiles user is following'''
-        
+        '''List of all profiles user is following'''
+        return self.following.all()
 
-    def follow(self):
-        '''Create following relationship between profiles'''
+    def follow(self, user_prof):
+        '''Create following relationship between profiles, can't add self'''
+        if self not in user_prof.blocking.all():
+            self.following.add(user_prof)
 
-    def unfollow(self):
+    def unfollow(self, user_prof):
         '''Remove following relationship between profiles'''
+        self.following.remove(user_prof)
 
+    def block(self, user_prof):
+        '''Create blocking realtionship'''
+        self.blocking.add(user_prof)
+
+    def unblock(self, user_prof):
+        '''Unblock user relationship'''
+        self.blocking.remove(user_prof)
