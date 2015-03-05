a9da857d104874d418fcd69a0752f828fdd7291f
Nicholas Draper <ndraper2@gmail.com>
2015-03-05 15:56:38 -0800

    started album model in imager_images

--- imager_images/models.py
+++ imager_images/models.py
@@ -11,5 +11,47 @@ class Photo(models.Model):
     date_modified = models.DateField(auto_now=True, null=True, blank=True)
     date_publishd = models.DateField(null=True, blank=True)
 
+    PRIVATE = 'private'
+    SHARED = 'shared'
+    PUBLIC = 'public'
+    PHOTO_PRIVACY_OPTIONS = (
+        (PRIVATE, 'private', ),
+        (SHARED, 'shared', ),
+        (PUBLIC, 'public', ),
+    )
 
-class Album(models.Model)
+    PUBLISHED = models.CharField(
+        max_length=3,
+        choices=PHOTO_PRIVACY_OPTIONS,
+        default=PRIVATE
+    )
+
+
+class Album(models.Model):
+    user = models.ForeignKey(User, related_name='album')
+    pictures = models.ManyToManyField(
+        Photo,
+        related_name='album',
+        limit_choices_to={user: 'self'}
+    )
+    title = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    date_created = models.DateField(auto_now_add=True, null=True, blank=True)
+    date_modified = models.DateField(auto_now=True, null=True, blank=True)
+    date_publishd = models.DateField(null=True, blank=True)
+
+    PRIVATE = 'private'
+    SHARED = 'shared'
+    PUBLIC = 'public'
+    PHOTO_PRIVACY_OPTIONS = (
+        (PRIVATE, 'private', ),
+        (SHARED, 'shared', ),
+        (PUBLIC, 'public', ),
+    )
+
+    PUBLISHED = models.CharField(
+        max_length=3,
+        choices=PHOTO_PRIVACY_OPTIONS,
+        default=PRIVATE
+    )
+    cover = models.ForeignKey(Photo)
