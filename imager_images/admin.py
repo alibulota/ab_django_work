19aafb18c61edb1f7165373fdcd6d3abf225d62d
Alicija Bulota <aabulota@smcm.edu>
2015-03-11 21:49:42 -0700

    added the new user profile model

--- imager_images/admin.py
+++ imager_images/admin.py
@@ -14,6 +14,32 @@ class ImagerProfileInline(admin.StackedInline):
 class UserAdmin(UserAdmin):
     inlines = (ImagerProfileInline, )
 
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.models import User
+# from dimager.models import ImagerProfile
+# from dimager.models import UserAdmin
+# from dimager.models import profileAdmin
+
+
+# class ImagerProfileInline(admin.StackedInline):
+#     model = ImagerProfile
+#     can_delete = False
+#     verbose_name_plural = 'ImagerProfile'
+
+
+# class UserAdmin(UserAdmin):
+#     inlines = (ImagerProfileInline, )
+
+
+# class profileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'is_active')
+
+
+# admin.site.unregister(User)
+# admin.site.register(User, UserAdmin)
+# admin.site.register(ImagerProfile, profileAdmin)
+
 
 admin.site.register(Album)
 admin.site.register(Photo)
