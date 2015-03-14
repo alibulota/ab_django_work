58da4625fd81a6fd66bdde816e2bdd3853fb9376
Jakealope <jake.anderson486@gmail.com>
2015-03-14 11:45:06 -0700

    added urls for stream profile and library

--- imager/urls.py
+++ imager/urls.py
@@ -18,20 +18,27 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='auth_login'),
-    
 
     url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'}, name='auth_logout'),
 
     url(r'^password/change/$', auth_views.password_change, {'post_change_redirect': reverse_lazy('auth_password_change_done')}, name='auth_password_change'),
 
     url(r'^password/change/done/$', auth_views.password_change_done, name='auth_password_change_done'),
-    
 
     url(r'^password/reset/$', auth_views.password_reset, {'post_reset_redirect': reverse_lazy('auth_password_reset_done')}, name='auth_password_reset'),
-   
 
     url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='auth_password_reset_complete'),
-   
+
 
     url(r'^password/reset/done/$', auth_views.password_reset_done, name='auth_password_reset_done'),
+    url(r'^library/(?P<id>\d+)', 'imager.views.library', name='library'),
+
+    url(r'^stream/(?P<id>\d+)', 'imager.views.stream', name='stream'),
+
+    url(r'^$', 'imager.views.home', name='home'),
+
+    url(r'^profile/', include('imagerprofile.urls')),
+
+    url(r'^images/', include('imager_images.urls')),
+
 )
