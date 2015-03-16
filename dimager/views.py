2cd32ef09aec17405dc94ec729d5e28eaca31322
Alicija Bulota <aabulota@smcm.edu>
2015-03-16 14:38:46 -0700

    added view for profile with name, birthday etc.

--- dimager/views.py
+++ dimager/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from dimager.models import ImagerProfile
+from imager_images.models import Album, Photo
+from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def user_profile(request):
-    context = {'name' : request.user}
-    return render(request, 'profile.html', context)
+    user = request.user
+    try:
+        albums = Album.objects.filter(user=user).all()
+    except:
+        albums = None
+    profile = ImagerProfile.objects.get(user=user)
+
+    num_photos = Photo.objects.filter(user=user).count()
+    return render(request, 'profile.html', {
+        'albums': albums,
+        'user': user,
+        'profile': profile,
+        'num_photo': num_photos
+    })
