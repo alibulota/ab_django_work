0be847d0b91b626ae24a0510ee3ba8a62f889b88
Alicija Bulota <aabulota@smcm.edu>
2015-03-16 15:24:27 -0700

    added view for library and user premission

--- dimager/views.py
+++ dimager/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from dimager.models import ImagerProfile
-from imager_images.models import Album, Photo
+from imager_images.models import Album, Photo, User
 from django.contrib.auth.decorators import login_required
 
 
-@login_required
-def user_profile(request):
-    user = request.user
+@login_required()
+def user_profile(request, id):
+    user = User.objects.get(id=id)
     try:
         albums = Album.objects.filter(user=user).all()
     except:
         albums = None
     profile = ImagerProfile.objects.get(user=user)
-
     num_photos = Photo.objects.filter(user=user).count()
     return render(request, 'profile.html', {
         'albums': albums,
@@ -23,8 +22,26 @@ def user_profile(request):
     })
 
 
-def library(request, id):
-    user = user_profile.objects.get(pk=id)
+@login_required()
+def user_profile(request):
+    user = request.user
+    try:
+        albums = Album.objects.filter(user=user).all()
+    except:
+        albums = None
+    profile = ImagerProfile.objects.get(user=user)
+    num_photos = Photo.objects.filter(user=user).count()
+    return render(request, 'profile.html', {
+        'albums': albums,
+        'user': user,
+        'profile': profile,
+        'num_photo': num_photos
+    })
+
+
+@login_required()
+def library(request):
+    user = request.user_profile
     try:
         albums = Album.objects.filter(user=user).all()
     except:
