67fe7b4680523204cd2711b2e63ad72090ac68ad
Jakealope <jake.anderson486@gmail.com>
2015-03-16 16:05:44 -0700

    adjusted profile views

--- dimager/views.py
+++ dimager/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from dimager.models import ImagerProfile
 from imager_images.models import Album, Photo, User
 from django.contrib.auth.decorators import login_required
+from django.views.generic import UpdateView
 
 
 @login_required()
@@ -55,3 +56,25 @@ def library(request):
         'albums': albums,
         'photos': photos
     })
+
+
+class ImagerProfileUpdateView(UpdateView):
+    model = ImagerProfile
+    template_name_suffix = '_update_form'
+    success_url = '/profile/'
+    fields = [
+        'profile_image',
+        'phone_number',
+        'birthday',
+        'phone_privacy',
+        'birthday_privacy',
+        'picture_privacy',
+        'name_privacy',
+        'email_privacy',
+        'following',
+        'blocking',
+    ]
+
+    def get_queryset(self):
+        qs = super(ImagerProfileUpdateView, self).get_queryset()
+        return qs.filter(user=self.request.user)
