e33eb73ea6e69da861b4e6b868c635bc02db3008
Jakealope <jake.anderson486@gmail.com>
2015-03-16 16:05:23 -0700

    added profile url

--- dimager/urls.py
+++ dimager/urls.py
@@ -1,7 +1,14 @@
-# from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
+from imagerprofile.views import (ImagerProfileUpdateView, profile,
+                                 user_profile, library, stream)
 
-
-# urlpatterns = patterns('',
-    
-#                        url(r'^$', 'imager_user.views.profile', name='profile'),
-#                        )
+urlpatterns = patterns('',
+    url(r'^$', profile, name='my_profile'),
+    url(r'^library/$', library, name='library'),
+    url(r'^stream/$', stream, name='stream'),
+    url(r'^(?P<id>\d+)/$', user_profile, name='profile'),
+    url(r'^(?P<pk>\d+)/edit/$',
+        login_required(ImagerProfileUpdateView.as_view()),
+        name='profile_edit'),
+)
