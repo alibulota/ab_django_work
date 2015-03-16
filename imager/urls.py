28ae7030e224e4a4b972f12474a0995b574e97ff
Alicija Bulota <aabulota@smcm.edu>
2015-03-16 13:59:51 -0700

    merge conflict

--- imager/urls.py
+++ imager/urls.py
@@ -18,28 +18,18 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='auth_login'),
-<<<<<<< HEAD
-=======
-
->>>>>>> bbb30caf14c9cb0dbdbd6df2c650993034061c9a
 
     url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'}, name='auth_logout'),
 
     url(r'^password/change/$', auth_views.password_change, {'post_change_redirect': reverse_lazy('auth_password_change_done')}, name='auth_password_change'),
 
     url(r'^password/change/done/$', auth_views.password_change_done, name='auth_password_change_done'),
-<<<<<<< HEAD
-
-    url(r'^password/reset/$', auth_views.password_reset, {'post_reset_redirect': reverse_lazy('auth_password_reset_done')}, name='auth_password_reset'),
-=======
-
 
     url(r'^password/reset/$', auth_views.password_reset, {'post_reset_redirect': reverse_lazy('auth_password_reset_done')}, name='auth_password_reset'),
 
->>>>>>> bbb30caf14c9cb0dbdbd6df2c650993034061c9a
+    url(r'^password/reset/$', auth_views.password_reset, {'post_reset_redirect': reverse_lazy('auth_password_reset_done')}, name='auth_password_reset'),
 
     url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='auth_password_reset_complete'),
-
 
     url(r'^password/reset/done/$', auth_views.password_reset_done, name='auth_password_reset_done'),
     url(r'^library/(?P<id>\d+)', 'imager.views.library', name='library'),
