068b9be77bff559ebf3f5738acb5bf4e3d8926ed
Jakealope <jake.anderson486@gmail.com>
2015-03-16 17:01:16 -0700

    added media root url

--- imager/settings.py
+++ imager/settings.py
@@ -92,6 +92,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "imager/static"),
 )
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 TEMPLATES_DIRS = [
     os.path.join(BASE_DIR, "/django-imager/imager/templates"),
 ]
