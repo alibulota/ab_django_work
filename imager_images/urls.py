619f90b570b8f3fa380f91113a93a1bbb4199501
Jakealope <jake.anderson486@gmail.com>
2015-03-18 18:20:12 -0700

    added urls

--- imager_images/urls.py
+++ imager_images/urls.py
@@ -1,8 +1,16 @@
-# urlpatterns = patterns('',
-#     url(r'^$', views.index, name='index'),
-#     url(r'^about/$', views.about, name='about'),
-#     url(r'^category/(?P<category_name_url>\w+)$', views.category, name='category'),
-#     url(r'^add_category/$', views.add_category, name='add_category'),
-#     url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),
-#     url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!
-#     )
+from django.conf.urls import patterns, url
+from views import Add_Photo, Add_Album
+from django.contrib.auth.decorators import login_required
+from imager_images.views import PhotoEditUpdateView, AlbumEditUpdateView
+
+
+urlpatterns = patterns('',
+                       url(r'^photo/add/$', login_required(Add_Photo.as_view()),
+                           name='photo_add'),
+                       url(r'^album/add/$', login_required(Add_Album.as_view()),
+                           name='album_add'),
+                       url(r'^photo/edit/(?P<pk>\d+)/$', login_required(PhotoEditUpdateView.as_view()), 
+                           name='photo_edit'),
+                       url(r'^album/edit/(?P<pk>\d+)/$', AlbumEditUpdateView.as_view(), 
+                           name='album_edit'),
+                       )
