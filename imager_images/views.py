5dd11a619bf0c3e4a77279547a402e69363e73e7
Jakealope <jake.anderson486@gmail.com>
2015-03-19 18:28:35 -0700

    corrected incorrect names

--- imager_images/views.py
+++ imager_images/views.py
@@ -1,56 +1,55 @@
-# from django.shortcuts import render
-# from dimager.forms import UserForm, ImagerProfileForm
-# from imager_images import profile_image
+from django.shortcuts import render
+from imager_images.forms import UserForm, ImagerProfileForm
+# from media import profile_image
 from django.core.urlresolvers import reverse_lazy
 from models import Photo, Album
 from django.views.generic import CreateView, UpdateView
-# from django.contrib.auth.decorators import login_required
-# from django.contrib.auth.views import redirect_to_login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import redirect_to_login
 from django.shortcuts import redirect
 from dimager.models import ImagerProfile
 
 
-# def register(request):
-#     context = RequestContext(request)
-#     registered = False
+def register(request):
+    context = RequestContext(request)
+    registered = False
 
-#     if request.method == 'POST':
-#         user_form = UserForm(data=request.POST)
-#         profile_form = ImagerProfileForm(data=request.POST)
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = ImagerProfileForm(data=request.POST)
 
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user = user_form.save()
-#             user.set_password(user.password)
-#             user.save()
-#             profile = profile_form.save(commit=False)
-#             profile.user = user
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
 
-#             if 'picture' in request.FILES:
-#                 profile.profile_image = request.FILES
-#                 profile.profile_image = request.FILES['picture']
+            if 'picture' in request.FILES:
+                profile.profile_image = request.FILES
+                profile.profile_image = request.FILES['picture']
 
-#             profile.save()
-#             registered = True
+            profile.save()
+            registered = True
 
-#         else:
-#             print user_form.errors, profile_form.errors
+        else:
+            print user_form.errors, profile_form.errors
 
-#     else:
-#         user_form = UserForm()
-#         profile_form = ImagerProfileForm()
+    else:
+        user_form = UserForm()
+        profile_form = ImagerProfileForm()
 
-#     return render_to_response(
-#         'dimager/imager/templates/registration/home.html')
+    return render_to_response(
+        'dimager/imager/templates/registration/home.html')
 
 
 class AddPhoto(CreateView):
     model = Photo
-    fields = ['title', 'description', 'image', 'published']
+    fields = ['title', 'description', 'picture', 'PUBLISHED']
     template_name = 'photo_add.html'
 
     def form_valid(self, form):
-        form.instance.profile = ImagerProfile.objects.get(
-            pk=self.request.user.profile.id)
+        form.instance.user = self.request.user
         return super(AddPhoto, self).form_valid(form)
 
     success_url = reverse_lazy('library')
@@ -58,7 +57,7 @@ class AddPhoto(CreateView):
 
 class PhotoEditUpdateView(UpdateView):
     model = Photo
-    fields = ['title', 'description', 'published']
+    fields = ['title', 'description', 'PUBLISHED']
     template_name = 'photo_edit.html'
 
     def dispatch(self, request, *args, **kwargs):
@@ -72,7 +71,7 @@ class PhotoEditUpdateView(UpdateView):
 
 class AlbumEditUpdateView(UpdateView):
     model = Photo
-    fields = ['title', 'description', 'photos', 'cover', 'published']
+    fields = ['title', 'description', 'pictures', 'cover', 'PUBLISHED']
     template_name = 'album_edit.html'
 
     def dispatch(self, request, *args, **kwargs):
@@ -86,7 +85,7 @@ class AlbumEditUpdateView(UpdateView):
 
 class AddAlbum(CreateView):
     model = Album
-    fields = ['title', 'description', 'photos', 'cover', 'published']
+    fields = ['title', 'description', 'pictures', 'cover', 'PUBLISHED']
     template_name = 'album_add.html'
 
     def form_valid(self, form):
