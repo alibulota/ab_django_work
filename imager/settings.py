aecfefcffa3064309aefcb359523b7dcd2b756eb
Jakealope <jake.anderson486@gmail.com>
2015-03-19 18:26:43 -0700

    removed contrib sites

--- imager/settings.py
+++ imager/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'dimager',
     'imager_images',
     'registration',
