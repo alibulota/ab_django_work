479412f939308ae7c58391f4bec908bb130b4be2
Alicija Bulota <aabulota@smcm.edu>
2015-04-02 16:24:37 -0700

    changes for deployment

--- imager/settings.py
+++ imager/settings.py
@@ -7,102 +7,92 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
+from configurations import Settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-USER_NAME = os.environ.get('USER')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+class Base(Settings):
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    USER_NAME = os.environ.get('USER')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mj)xs(k)0^9xt!ah-0nc^&03l%cpsq2!p#hq9)3i2why&ynyd('
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = 'mj)xs(k)0^9xt!ah-0nc^&03l%cpsq2!p#hq9)3i2why&ynyd('
 
-TEMPLATE_DEBUG = True
+    # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = []
+    ALLOWED_HOSTS = []
 
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'dimager',
+        'imager_images',
+        'registration',
 
-# Application definition
+    )
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'dimager',
-    'imager_images',
-    'registration',
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-)
+    ROOT_URLCONF = 'imager.urls'
+    WSGI_APPLICATION = 'imager.wsgi.application'
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'imager.urls'
-
-WSGI_APPLICATION = 'imager.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangoimager',
-        'USER': 'aabulota',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'djangoimager',
+            'USER': 'aabulota',
+        }
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+    LANGUAGE_CODE = 'en-us'
+    TIME_ZONE = 'UTC'
+    USE_I18N = True
+    USE_L10N = True
+    USE_TZ = True
 
-LANGUAGE_CODE = 'en-us'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "imager/static"),
+    )
+    TEMPLATES_DIRS = [
+        os.path.join(BASE_DIR, "/django-imager/imager/templates"),
+    ]
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    ACCOUNT_ACTIVATION_DAYS = 3
+    REGISTRATION_AUTO_LOGIN = True
+    REGISTRATION_OPEN = True
+    LOGIN_URL = '/accounts/login/'
+    LOGIN_REDIRECT_URL = '/profile/'
 
-TIME_ZONE = 'UTC'
 
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+class Dev(Base):
+    DEBUG = True
+    TEMPLATE_DEBUG = True
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "imager/static"),
-)
-
-TEMPLATES_DIRS = [
-    os.path.join(BASE_DIR, "/django-imager/imager/templates"),
-]
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-ACCOUNT_ACTIVATION_DAYS = 3
-
-REGISTRATION_AUTO_LOGIN = True
-
-REGISTRATION_OPEN = True
-
-LOGIN_URL = '/accounts/login/'
-
-LOGIN_REDIRECT_URL = '/profile/'
+class Prod(Base):
+    import dj_database_url
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    DEBUG = False
+    TEMPLATE_DEBUG = False
+    DATABASES = {
+                'default': dj_database_url.config()
+                }
