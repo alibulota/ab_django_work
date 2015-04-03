807b1655a527c470fa5b6a5cc5a71ab0bd960b96
Alicija Bulota <aabulota@smcm.edu>
2015-04-02 21:29:59 -0700

    added more missing files

--- imager/urls.py
+++ imager/urls.py
@@ -7,41 +7,54 @@ from imager import views as main_views
 from registration.backends.simple.views import RegistrationView
 from django.conf import settings
 
+
 class MyRegistrationView(RegistrationView):
     def get_success_url(self, request, user):
         return '/profile/'
 
 
-urlpatterns = patterns('',
-    url(r'^$', 'imager.views.home', name = 'home'),
+urlpatterns = patterns(
+      '',
+      url(r'^$', main_views.home, name='home'),
+      # url(r'^blog/', include('blog.urls')),
+      url(r'^', include('imager_images.urls')),
 
-    url(r'^profile/', 'dimager.views.user_profile'),
+      url(r'^accounts/', include('registration.backends.simple.urls')),
 
-    url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+      url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='auth_login'),
-    
+      url(r'^profile/', 'dimager.views.user_profile'),
 
-    url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logout.html'}, name='auth_logout'),
+      url(r'^login/$', auth_views.login,
+          {'template_name': 'registration/login.html'}, name='auth_login'),
 
-    url(r'^password/change/$', auth_views.password_change, {'post_change_redirect': reverse_lazy('auth_password_change_done')}, name='auth_password_change'),
+      url(r'^logout/$', auth_views.logout,
+          {'template_name': 'registration/logout.html'}, name='auth_logout'),
 
-    url(r'^password/change/done/$', auth_views.password_change_done, name='auth_password_change_done'),
-    
+      url(r'^password/change/$', auth_views.password_change,
+          {'post_change_redirect': reverse_lazy('auth_password_change_done')},
+          name='auth_password_change'),
 
-    url(r'^password/reset/$', auth_views.password_reset, {'post_reset_redirect': reverse_lazy('auth_password_reset_done')}, name='auth_password_reset'),
-   
+      url(r'^password/change/done/$', auth_views.password_change_done,
+          name='auth_password_change_done'),
 
-    url(r'^password/reset/complete/$', auth_views.password_reset_complete, name='auth_password_reset_complete'),
-   
+      url(r'^password/reset/$', auth_views.password_reset,
+          {'post_reset_redirect': reverse_lazy('auth_password_reset_done')},
+          name='auth_password_reset'),
 
-    url(r'^password/reset/done/$', auth_views.password_reset_done, name='auth_password_reset_done'),
+      url(r'^password/reset/complete/$', auth_views.password_reset_complete,
+          name='auth_password_reset_complete'),
 
-    url(r'^library/', 'dimager.views.library', name='library'),
+      url(r'^password/reset/done/$', auth_views.password_reset_done,
+          name='auth_password_reset_done'),
 
-    url(r'^stream/', 'dimager.views.stream'),
-)
+      url(r'^library/', 'dimager.views.library', name='library'),
+      url(r'^stream/', 'dimager.views.stream'),
 
+      # url(r'^photo/add', 'imager_images.views.AddPhoto'),
+
+      # url(r'^album/add', 'imager_images.views.AddAlbum'),
+
+       )
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- imager_images/views.py
+++ imager_images/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from dimager.forms import UserForm, ImagerProfileForm
+from imager_images.forms import UserForm, ImagerProfileForm
+# from media import profile_image
+from django.core.urlresolvers import reverse_lazy, reverse
+from models import Photo, Album
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import redirect_to_login
+from django.shortcuts import redirect
+from dimager.models import ImagerProfile
 
 
 def register(request):
@@ -18,8 +26,8 @@ def register(request):
             profile.user = user
 
             if 'picture' in request.FILES:
-                profile.picture = request.FILES:
-                profile.picture = request.FILES['picture']
+                profile.profile_image = request.FILES
+                profile.profile_image = request.FILES['picture']
 
             profile.save()
             registered = True
@@ -33,3 +41,59 @@ def register(request):
 
     return render_to_response(
         'dimager/imager/templates/registration/home.html')
+
+
+class AddPhoto(CreateView):
+    model = Photo
+    fields = ['title', 'description', 'picture', 'PUBLISHED']
+    template_name = 'photo_add.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddPhoto, self).form_valid(form)
+
+    # success_url = reverse_lazy('library')
+
+    def get_success_url(self):
+        return reverse('library')
+
+class PhotoEditUpdateView(UpdateView):
+    model = Photo
+    fields = ['title', 'description', 'PUBLISHED']
+    template_name = 'photo_edit.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        photo_profile = Photo.objects.get(id=int(self.kwargs['pk'])).profile
+        user_profile = ImagerProfile.objects.get(user=self.request.user)
+        if photo_profile != user_profile:
+            return redirect('/accounts/login/')
+        return super(PhotoEditUpdateView, self).dispatch(request, *args, **kwargs)
+    # success_url = reverse_lazy('library')
+
+    
+
+
+class AlbumEditUpdateView(UpdateView):
+    model = Photo
+    fields = ['title', 'description', 'pictures', 'cover', 'PUBLISHED']
+    template_name = 'album_edit.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        album_profile = Album.objects.get(id=int(self.kwargs['pk'])).profile
+        user_profile = ImagerProfile.objects.get(user=self.request.user)
+        if album_profile != user_profile:
+            return redirect('/accounts/login/')
+        return super(AlbumEditUpdateView, self).dispatch(request, *args, **kwargs)
+    success_url = reverse_lazy('library')
+
+
+class AddAlbum(CreateView):
+    model = Album
+    fields = ['title', 'description', 'pictures', 'cover', 'PUBLISHED']
+    template_name = 'album_add.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddAlbum, self).form_valid(form)
+
+    success_url = reverse_lazy('library')
